905e66ad93110d355a7f9ef0f63d7031f4029712
elda27 <kaz.birdstick@gmail.com>
2021-08-12 21:48:39 +0900

    allow import *

--- finjet/__init__.py
+++ finjet/__init__.py
@@ -1,5 +1,14 @@
-from finjet.functional import get_global_container, inject  # NOQA: F401
-from finjet.dependency import Dependency, Depends, Singleton  # NOQA: F401
-from finjet.container import Container  # NOQA: F401
+from finjet.functional import get_global_container, set_global_container, inject
+from finjet.dependency import Dependency, Depends, Singleton
+from finjet.container import Container
 
 __version__ = '0.1.2'
+__all__ = [
+    get_global_container,
+    set_global_container,
+    inject,
+    Dependency,
+    Depends,
+    Singleton,
+    Container
+]
