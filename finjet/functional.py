ed91d5a8e790bb5507b1c9b4546303aa072fbb7f
elda27 <kaz.birdstick@gmail.com>
2021-08-12 21:08:12 +0900

    Modify setter for global container

--- finjet/functional.py
+++ finjet/functional.py
@@ -4,12 +4,23 @@ from typing import Any, Callable, Optional, TypeVar
 from finjet.container import Container
 
 
-def get_global_container() -> Optional[Container]:
-    """get global container object
+def set_global_container(container: Container) -> None:
+    """Set global container object
 
     Returns
     -------
     Optional[Container]
+        Container object
+    """
+    Container.current = container
+
+
+def get_global_container() -> Container:
+    """get global container object
+
+    Returns
+    -------
+    Container
         Container object
     """
     if Container.current is None:
